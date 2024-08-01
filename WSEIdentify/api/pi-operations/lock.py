38bcacf8c607f402a53308fce66d2bb51495a2a9
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 10:19:18 -0400

    Reimplemented locking system

--- WSEIdentify/api/pi-operations/lock.py
+++ WSEIdentify/api/pi-operations/lock.py
@@ -3,8 +3,7 @@ import time
 import random
 from signal import pause
 
-contactorButton = gpiozero.Button(4, pull_up=True)
-contactorLED = gpiozero.LED(21)
+contactorButton = gpiozero.LED(21, pull_up=True)
 
 print(contactorButton)
 print(contactorLED)
@@ -13,5 +12,3 @@ try:
    print("s:Contactor is deactivated!")
 except Exception as e:
    print("e:Error in turning off contactor")
-
-pause()
