b03321679afc45296296982ee3155cfbe4e99afd
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 10:33:17 -0400

    a

--- WSEIdentify/api/pi-operations/pi-test1.py
+++ WSEIdentify/api/pi-operations/pi-test1.py
@@ -4,16 +4,16 @@ led = gpiozero.LED(20)
 detectionLed = gpiozero.LED(26)
 
 keep_active = False
+pulse_started = False
 
 while True:
-    if detectionLed.is_lit and not keep_active:
-        keep_active = True
-        print("The LED connected to GPIO26 is active!")
-    elif not detectionLed.is_lit and keep_active:
-        keep_active = False
-        print("The LED connected to GPIO26 has been deactivated!")
-
-    if keep_active:
-        led.on()
-    else:
-        led.off()
+    if detectionLed.is_lit:
+        pulse_started = True
+    if not detectionLed.is_lit and pulse_started:
+        pulse_started = False
+        print("Pulse received! Toggling GPIO20")
+        if led.is_lit:
+            led.off()
+        else:
+            led.on()
+    
