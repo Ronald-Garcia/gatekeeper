3985a0a67a8f427f1b4f31662ad847acb8c1fca0
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 12:18:18 -0400

    Finalized pi-operations

--- WSEIdentify/api/src/server.ts
+++ WSEIdentify/api/src/server.ts
@@ -4,6 +4,8 @@ import app from './app'
 const port = parseInt(process.env.PORT as string) || 3000
 console.log(`Server is running on port ${port}`)
 
+// TODO:
+// consider automatically removing trainings of some people after some time
 // run server
 serve({
   fetch: app.fetch,
