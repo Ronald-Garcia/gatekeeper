67bea0596f81299df147166ec29f82b5b5d3fba8
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 10:22:57 -0400

    Reimplemented locking system

--- WSEIdentify/api/pi-operations/pi-test1.py
+++ WSEIdentify/api/pi-operations/pi-test1.py
@@ -1,16 +1,17 @@
 import gpiozero
 
 led = gpiozero.LED(20)
+detectionLed = gpiozero.LED(26)
 
 keep_active = False
 
 while True:
-    if led.is_lit and not keep_active:
+    if detectionLed.is_lit and not keep_active:
         keep_active = True
-        print("The LED connected to GPIO20 is active!")
-    elif not led.is_lit and keep_active:
+        print("The LED connected to GPIO26 is active!")
+    elif not detectionLed.is_lit and keep_active:
         keep_active = False
-        print("The LED connected to GPIO20 has been deactivated!")
+        print("The LED connected to GPIO26 has been deactivated!")
 
     if keep_active:
         led.on()
