f1bad73545160e6a618a405e511bf308f6c66562
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 10:53:39 -0400

    Added the python scripts

--- WSEIdentify/api/pi-operations/lock.py
+++ WSEIdentify/api/pi-operations/lock.py
@@ -5,9 +5,6 @@ from signal import pause
 
 contactorLED = gpiozero.LED(21)
 
-print(contactorLED)
-try:
-   contactorLED.off()
-   print("s:Contactor is deactivated!")
-except Exception as e:
-   print("e:Error in turning off contactor")
+contactorLED.on()
+time.sleep(0.1)
+contactorLED.off()
--- WSEIdentify/api/pi-operations/unlock.py
+++ WSEIdentify/api/pi-operations/unlock.py
@@ -4,17 +4,9 @@ import random
 from signal import pause
 
 contactorLED = gpiozero.LED(21)
-try:
-   contactorLED.on()
-   if contactorLED.is_lit:
-      print("s:Contactor is activated!")
-except Exception as e:
-   print("e:Error in turning on contactor")
-try:
-    contactorLED.close()
-    time.sleep(1)
-    contactorLED = gpiozero.LED(21) # pin 40
-    contactorLED.on()
-    print("s:Contactor is activated!")
-except Exception as e:
-    print("e:Could not turn on contactor.")
+contactorLED.on()
+time.sleep(0.1)
+contactorLED.off()
+time.sleep(0.1)
+
+print("s:Success!")
