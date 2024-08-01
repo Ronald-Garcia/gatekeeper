9136cdc9cdcf5e9eefd5938cdabf5e6d5a327f15
Ronald Garcia <rgarcia0303757@gmail.com>
2024-08-01 10:00:59 -0400

    Added test python scripts

---
+++ WSEIdentify/api/pi-operations/pi-test1.py
@@ -0,0 +1,9 @@
+from gpiozero import LED
+
+led = LED(21)
+
+while True:
+    if led.is_lit:
+        print("The LED connected to GPIO21 is active!")
+    else:
+        print("The LED connected to GPIO21 is deactivated!")
