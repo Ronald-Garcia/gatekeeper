20235b48e0838260f4ec6f2d0b37316028a1542a
Ronald Garcia <rgarcia0303757@gmail.com>
2024-09-04 09:26:53 -0400

    added dockerfiles

--- WSEIdentify/api/src/db/index.ts
+++ WSEIdentify/api/src/db/index.ts
@@ -2,8 +2,11 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 
+
+const DATABASE_FILE = process.env.DB_FILE || "sqlite.db";
+
 // Initialize the SQLite database and export the connection
-export const connection = new Database("sqlite.db");
+export const connection = new Database(DATABASE_FILE);
 
 // Create the database and export it
 export const db = drizzle(connection, { schema });
