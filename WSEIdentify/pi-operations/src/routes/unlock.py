75a262e5e3005f89b54792233037b75651ede084
Ronald Garcia <rgarcia0303757@gmail.com>
2024-09-18 10:50:13 -0400

    aa

--- WSEIdentify/pi-operations/src/routes/unlock.py
+++ WSEIdentify/pi-operations/src/routes/unlock.py
@@ -3,14 +3,10 @@ import time
 import random
 from signal import pause
 
-if gpiozero:
-    print("there is GPIOZERO")
+contactorLED = gpiozero.LED(21)
+contactorLED.on()
+time.sleep(0.1)
+contactorLED.off()
+time.sleep(5)
 
-try:
-    contactorLED = gpiozero.LED("GPIO12")
-    while True:
-        contactorLED.on()
-
-    print("s:Success!")
-except:
-    print("not success");
+print("s:Success!")
