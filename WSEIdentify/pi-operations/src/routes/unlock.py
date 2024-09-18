c23c7530a9f4f73b89bea7f2fffcdc1b96c827e4
Ronald Garcia <rgarcia0303757@gmail.com>
2024-09-18 10:38:44 -0400

    aa

--- WSEIdentify/pi-operations/src/routes/unlock.py
+++ WSEIdentify/pi-operations/src/routes/unlock.py
@@ -7,12 +7,9 @@ if gpiozero:
     print("there is GPIOZERO")
 
 try:
-
     contactorLED = gpiozero.LED(21)
-    contactorLED.on()
-    time.sleep(0.1)
-    contactorLED.off()
-    time.sleep(5)
+    while True:
+        contactorLED.off()
 
     print("s:Success!")
 except:
