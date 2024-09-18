6f9a2bc0f0012191ad869fbd226f611d4777c36f
Ronald Garcia <rgarcia0303757@gmail.com>
2024-09-18 10:34:29 -0400

    aa

--- WSEIdentify/pi-operations/src/routes/unlock.py
+++ WSEIdentify/pi-operations/src/routes/unlock.py
@@ -3,10 +3,11 @@ import time
 import random
 from signal import pause
 
+
 contactorLED = gpiozero.LED(21)
 contactorLED.on()
 time.sleep(0.1)
 contactorLED.off()
-time.sleep(0.1)
+time.sleep(5)
 
 print("s:Success!")
